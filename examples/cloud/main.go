8ad48e56bf49170e9702ed44b5020b9353613448
José Lico <lico.jose.pt@gmail.com>
2024-10-08 13:12:55 +0100

    Added Hello World app for cloud deployment

---
+++ examples/cloud/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/jose-lico/go-plate/api"
+	"github.com/jose-lico/go-plate/config"
+)
+
+func main() {
+	cfg := config.NewAPIConfig()
+	api := api.NewAPIServer(cfg)
+	api.UseDefaultMiddleware()
+
+	api.Router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	err := api.Run()
+	if err != nil {
+		log.Fatalf("[FATAL] Error launching API server: %v", err)
+	}
+}
